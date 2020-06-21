8cc29dede226a300c1abe873d84c5a2b52d8beab
Benjamin Diaz <benjamin.diaz@iunigo.com>
2020-06-21 18:47:54 -0300

    Adds display scale factor
    Signed-off-by: Benjamin Diaz <benjamin.diaz@iunigo.com>

--- src/drivers.rs
+++ src/drivers.rs
@@ -4,6 +4,10 @@ use sdl2::pixels::Color;
 use sdl2::rect::Point;
 use sdl2::Sdl;
 
+const CHIP8_WIDTH: u32 = 64;
+const CHIP8_HEIGHT: u32 = 32;
+const CHIP8_SCALE: u32 = 8;
+
 pub struct DisplayDriver {
     canvas: WindowCanvas
 }
@@ -12,7 +16,7 @@ impl DisplayDriver {
     pub fn new(sdl_context: &Sdl) -> Result<DisplayDriver, Box<dyn Error>> {
         let video_subsystem = sdl_context.video()?;
 
-        let window = video_subsystem.window("chip8", 64, 32)
+        let window = video_subsystem.window("chip8", CHIP8_WIDTH * CHIP8_SCALE, CHIP8_HEIGHT * CHIP8_SCALE)
             .position_centered()
             .build()
             .expect("could not initialize video subsystem");
@@ -30,15 +34,19 @@ impl DisplayDriver {
     pub fn draw(&mut self, gfx: [u8; 64 * 32]) {
         // TODO: Traverse gfx and draw pixels
         self.canvas.set_draw_color(Color::RGB(255, 255, 255));
-        let mut x;
-        let mut y;
+        let mut x:i32;
+        let mut y:i32;
         let mut p;
         for (i, elem) in gfx.iter().enumerate() {
             if *elem == 1 {
-                x = i % 64;
-                y = i / 64;
-                p = Point::new(x as i32, y as i32);
-                self.canvas.draw_point(p).expect("could not draw point");
+                x = ((i % CHIP8_WIDTH as usize) * CHIP8_SCALE as usize) as i32;
+                y = ((i / CHIP8_WIDTH as usize) * CHIP8_SCALE as usize) as i32;
+                for j in 0..CHIP8_SCALE {
+                    for k in 0..CHIP8_SCALE {
+                        p = Point::new((x + j as i32), (y + k as i32));
+                        self.canvas.draw_point(p).expect("could not draw point");
+                    }
+                }
             }
         }
         self.canvas.present();
